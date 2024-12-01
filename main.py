3423b94f87a82a432e24c5f8859b8e163422761a
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-12-01 09:49:20 +0200

    Python 3.14 is in alpha

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ Status = collections.namedtuple(
 
 MAJORS = {
     # version: (past_eol, alpha)
-    "3.14": Status(dev=True),
+    "3.14": Status(alpha=True),
     "3.13": Status(),
     "3.12": Status(),
     "3.11": Status(),
